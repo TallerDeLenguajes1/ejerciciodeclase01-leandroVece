965c220b957dc1dd7457b350ad677931dd1e9292
Leandro Suarez Vece <leovece1@gmail.com>
2021-08-18 19:05:37 -0300

    sin miedo al exito 2

--- taller2/Program.cs
+++ taller2/Program.cs
@@ -7,20 +7,33 @@ namespace taller2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            int num = 0;
+            int num, intento=0;
             Random rand = new Random();
 
-            while (rand.Next(100) == num)
+            do
             {
-                num = Convert.ToInt32(Console.ReadLine());
-                if (num < rand.Next(100))
-                    Console.WriteLine("El numero es menor");
-                if (num > rand.Next(100))
-                    Console.WriteLine("El numero es mayor");
+                try
+                {
+                    num = Convert.ToInt32(Console.ReadLine());
+                    if (num < rand.Next(100))
+                        Console.WriteLine("El numero es menor");
+                    if (num > rand.Next(100))
+                        Console.WriteLine("El numero es mayor");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Error al ingresar dato");
+                    num = -1;
+                }
+                finally
+                {
+                    Console.WriteLine("Intente nuevamente");
+                    intento++;
+                }
 
-            }
+            } while (rand.Next(100) != num);
 
-            Console.WriteLine("felicidades acerto");
+            Console.WriteLine("felicidades acerto en {0}", intento);
         }
     }
 }
