f369b53fdb136548d5b4731f3ef370f8b8b46cfc
Leandro Suarez Vece <leovece1@gmail.com>
2021-08-18 17:52:09 -0300

    sin miedo al exito

---
+++ taller2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace taller2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int num = 0;
+            Random rand = new Random();
+
+            while (rand.Next(100) == num)
+            {
+                num = Convert.ToInt32(Console.ReadLine());
+                if (num < rand.Next(100))
+                    Console.WriteLine("El numero es menor");
+                if (num > rand.Next(100))
+                    Console.WriteLine("El numero es mayor");
+
+            }
+
+            Console.WriteLine("felicidades acerto");
+        }
+    }
+}
